4538af9b7680e24ddead9344b3af6c8fa7a2213a
aliiyaaa <alfiagalidullaeva@gmail.com>
2025-09-27 22:38:34 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,19 +1,1 @@
-import toys.DollToy;
-import toys.Toy;
-import toys.CarToy;
-import adapter.BatteryAdapter;
-import remotecont.RemoteController;
 
-public class Main {
-    public static void main(String[] args) {
-
-        Toy car = new CarToy("Race Car");
-        car.RunsWithBattery(80);
-
-        Toy doll = new DollToy("Barbie");
-        doll.RunsWithBattery(100);
-
-        Toy remote = new BatteryAdapter(new RemoteController());
-        remote.RunsWithBattery(80);
-    }
-}
